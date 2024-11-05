aa3b06fe8908241720395b94e662b2e53ea08d92
CJ Steiner <clintonsteiner@gmail.com>
2024-11-04 19:02:01 -0600

    add in test_sphinx.py simple

---
+++ tests/test_sphinx.py
@@ -0,0 +1,5 @@
+import subprocess
+
+def test_sphinx_build():
+    result = subprocess.run(["sphinx-build", "-b", "html", "../source", "../build"], capture_output=True, text=True)
+    assert result.returncode == 0, f"Build failed with errors: {result.stderr}"
